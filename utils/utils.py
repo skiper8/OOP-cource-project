a1552ab796263e3a946ea53ddb161cb714fe8acd
askip <a.skiper2013@yandex.ru>
2023-03-30 23:26:35 +0300

    Доработка классов и постоение логики файла main

--- utils/utils.py
+++ utils/utils.py
@@ -1,17 +1,54 @@
-import os
+from datetime import datetime
 import json
-import csv
-
-import requests as requests
+from operator import itemgetter
 
 
-filename = '../vacant_HH.json'
+def get_count_of_vacancy(filename):
+    """
+    Вернуть количество вакансий от текущего сервиса.
+    Получать количество необходимо динамически из файла.
+    """
 
-with open(filename, 'r') as file:
-    o = json.loads(file.read())
+    sj_count = 0
+    hh_count = 0
+    with open(filename, 'r', encoding='UTF-8') as file:
+        vacancies_list = json.load(file)
+        for item in vacancies_list:
+            if item['source'] == "SuperJob":
+                sj_count += 1
+            if item['source'] == "HeadHunter":
+                hh_count += 1
+    print(f'На сайте SuperJob было найдено {sj_count} вакансий по вашему запросу. \n'
+          f'На сайте HeadHunter было найдено {hh_count} вакансий по вашему запросу. \n')
 
-name = o['name']
-url = o['url']
-salary_to = o['salary_to']
-salary_from = o['salary_from']
 
+def sorted_to_salary(filename):
+    """
+    Сортировка по ЗП динамически из файла
+    """
+
+    with open(filename, 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+        sort_to_salary = sorted(data, key=itemgetter('salary_to'), reverse=True)
+    return sort_to_salary
+
+
+def date_format(data):
+    """
+    Перевод даты в нужный формат
+    """
+    for i in data:
+        if i["source"] == "HeadHunter":
+            i['date'] = datetime.strptime(i['date'], '%Y-%m-%dT%H:%M:%S+%f').strftime('%d.%m.%Y')
+        elif i["source"] == "SuperJob":
+            i['date'] = datetime.utcfromtimestamp(i['date']).strftime('%d.%m.%Y')
+    return data
+
+
+def data_sorted(data):
+    """
+    Сортировка по дате
+    """
+
+    sort_to_data = sorted(data, key=itemgetter('date'), reverse=True)
+    return sort_to_data
